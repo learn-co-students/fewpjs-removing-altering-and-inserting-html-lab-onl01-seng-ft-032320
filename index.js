ea67a818cffb6e2dac3ff0b67ecae619f6e6220f
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-22 18:18:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let elementMain = document.querySelector("main#main");
+elementMain.remove();
+
+
+var newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Arv is the champion";
