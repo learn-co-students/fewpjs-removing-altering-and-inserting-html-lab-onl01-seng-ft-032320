78c7b0b4bf9b2408bd5c26598ea5c9b8135ed955
ferrisbueller66 <github email address>
2020-06-22 18:36:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove()
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.innerText = "Trevor is the champion"
