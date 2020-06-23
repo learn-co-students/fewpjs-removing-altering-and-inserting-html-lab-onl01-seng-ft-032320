53475d90ed013743be0d6b15df05bcd134757ab0
ronniekram <arynn.boniface@gmail.com>
2020-06-23 14:44:38 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+let m = document.querySelector('main#main');
+m.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+newHeader.className = "victory";
+newHeader.innerHTML = "Ronnie is the champion!";
+
+
+
+
