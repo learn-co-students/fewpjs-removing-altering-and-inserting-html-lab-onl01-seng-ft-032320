dcc49b9d11a6f6c1155f59e3fe5e5163cdd3613c
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-06-23 15:54:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.getElementById("main").remove();
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Wielfried is the champion"
+
+document.body.appendChild(newHeader);
