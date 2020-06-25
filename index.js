a226e6e6ec4a52f6ed04da76059513d699a14611
proiacm <proiacm@gmail.com>
2020-06-25 15:27:19 -0600

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ main.remove()
 
 let newHeader = document.createElement('h1')
 newHeader.id = 'victory';
-newHeader.innerHTML = 'MARIA is the champion'
+newHeader.innerHTML = 'MARIA is the champion';
