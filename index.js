323aefbda21076bc7c710ac204e94972d96e5161
rwparrish <rino3717@gmail.com>
2020-06-25 15:28:48 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove()
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Ryan is the champion'
+
+// let newHeader = document.createElementById("h1#victory");
+// newHeader.innerHTML = "<h1>Ryan is the champion</h1>";
